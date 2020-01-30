5e23da891b7863b6516c01ac143dc4a8d0a619eb
jhonmendez <jhonmendex@gmail.com>
2020-01-29 22:38:49 -0500

    0.1 creacion codigo base del plugin

--- __init__.py
+++ __init__.py
@@ -1,11 +1,20 @@
-class DemoPage(object):
-    """ A simple demo page showing how to add a new page """
+import os
 
-    def GET(self):
-        """ GET request """
-        return "This is a simple demo plugin"
+from inginious.frontend.plugins.utils import create_static_resource_page, read_file
+
+_static_folder_path = os.path.join(os.path.dirname(__file__), "static")
+_UNCOLAB_HTML_FILE = "user_colab.html"
 
 
-def init(plugin_manager, course_factory, client, plugin_config):
-    """ Plugin colaborativo """
-    plugin_manager.add_page("/uncolab", DemoPage)
+def init(plugin_manager, course_factory, client, config):
+    plugin_manager.add_page(r'/un_colab/static/(.*)', create_static_resource_page(_static_folder_path))
+
+    use_minified = config.get("use_minified", False)
+    if use_minified:
+        plugin_manager.add_hook("javascript_footer", lambda: "/un_colab/static/js/user_colab.min.js")
+        plugin_manager.add_hook("css", lambda: "/un_colab/static/css/user_colab.min.css")
+    else:
+        plugin_manager.add_hook("javascript_footer", lambda: "/un_colab/static/js/user_colab.js")
+        plugin_manager.add_hook("css", lambda: "/un_colab/static/css/user_colab.css")
+    plugin_manager.add_hook("additional_body_html",
+                            lambda: read_file(_static_folder_path, _UNCOLAB_HTML_FILE))
---
+++ static/js/user_colab.js
@@ -0,0 +1,27 @@
+
+$(function(){
+   //$("#wrapper-chat").prop("hidden", true);
+    function appendChatButton() {
+        // this function add button in the frontend task
+        const html = "<br><button onclick='appendChat()' id='menu-toggle' class='btn btn-secondary'><i id=\"toggleIcon\" class=\"fa fa-angle-double-down\"></i> Collaboration</button>";
+        let task_panel = $('#task');
+        task_panel.append(html);
+    }
+    appendChatButton();
+});
+
+     function appendChat(){
+         var isIE11 = !!navigator.userAgent.match(/Trident.*rv\:11\./);
+         $("#toggleIcon").toggleClass("fa fa-angle-double-down fa fa-angle-double-up")
+         $("#wrapper-chat").toggleClass("toggled");
+
+         if(isIE11){
+            if($("#wrapper-chat").hasClass("toggled")){
+             $('#sidebar-wrapper').css("margin-left", "-268px")
+            }
+            else {
+              $('#sidebar-wrapper-chat').css("margin-left", "-250px")
+            }
+        }
+     }
+
